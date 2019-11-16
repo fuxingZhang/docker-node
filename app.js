3bb198aa723aff9ebe19c83efb0807472995c6a9
star <502545703@qq.com>
2019-11-16 16:03:31 +0800

    add code

---
+++ app.js
@@ -0,0 +1,28 @@
+const Koa = require('koa');
+const app = new Koa();
+const port = 3000;
+const host = '0.0.0.0';
+
+// logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// response
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(port, host, () => {
+  console.log(`Running on http://${host}:${port}`);
+});
